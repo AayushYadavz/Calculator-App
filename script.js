59e6c11d01110e21548afdf7b2c6791d8f387b74
Ayush Yadav <ayushyadav24nov1234@gmail.com>
2024-06-28 17:19:08 +0530

    Added comments in Js file to better understand the code.

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 const display = document.getElementById('display')
 
+// To clear input feild
 function clearDisplay(){
     display.value = ''
 }
 
+// To delete last character from input feild
 function deleteLastCharacter(){
     display.value = (display.value).toString().slice(0, -1)
 }
 
+// To add button digits and operators in input feild
 function appendToDisplay(val){
     display.value += val
 }
 
+// For calculating on clicking "=" button
 function calculateResult() {
     try {
         display.value = eval(display.value);
